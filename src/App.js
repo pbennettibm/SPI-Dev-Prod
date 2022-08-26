1eb3662b83dcb736d554ec0f68f6fc24ec6ac574
pbennettibm <Patrick.Bennett@ibm.com>
2022-08-25 20:12:11 -0500

    add text & style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { withWebChat } from '@ibm-watson/assistant-web-chat-react';
 import CustomResponsePortalsContainer from './CustomResponsePortalsContainer';
+import './App.css';
+import { ReactComponent as HomeDepotLogo } from './images/home-depot.svg';
+import { ReactComponent as WatsonLogo } from './images/watson.svg';
 
 const App = ({ createWebChatInstance }) => {
   const [instance, setInstance] = useState(null);
@@ -22,6 +25,27 @@ const App = ({ createWebChatInstance }) => {
 
   return (
     <div className='app'>
+      <div className='main-container'>
+        <div className='top-container'>
+          <HomeDepotLogo />
+          <h1>WELCOME!</h1>
+          <div className='powered-by-container'>
+            <div className='powered-by-text'>Powered by:&nbsp;</div>
+            <WatsonLogo className='watson' />
+          </div>
+        </div>
+        <div className='bottom-container'>
+          <div className='intro-text'>
+            I'm an assistant for the Home Depot's Technology Deployment Center.
+            I'm here to chat with and ask questions about your orders with the
+            TDC. I can answer questions and send emails to support services to
+            aid an any issues that you may be facing.
+          </div>
+          <div className='intro-text-bold'>
+            Click or tap on the icon in the corner to get started!
+          </div>
+        </div>
+      </div>
       {instance && <CustomResponsePortalsContainer instance={instance} />}
     </div>
   );
