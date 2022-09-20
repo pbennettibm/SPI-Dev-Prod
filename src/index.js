50638bc26611f2aac3e0ec8a5e3bf7dc40d60ae5
pbennettibm <Patrick.Bennett@ibm.com>
2022-09-20 13:49:42 -0500

    add wa json backup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// st
 root.render(<App />);
 
 reportWebVitals();
